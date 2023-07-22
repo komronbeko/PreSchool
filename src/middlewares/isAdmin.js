4d02cca391c337a45705ba50ea736074c2b89c4e
kolimov0825@gmail.com <kolimov0825@gmail.com>
2023-07-22 10:25:19 +0500

    middlewares fixed

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,1 +1,10 @@
+const isAdmin = async (req, res, next) => {
+  const data = req.verified;
+  console.log(data);
+  if (data?.role != "admin") {
+    return res.status(400).json({ message: "You are not admin" });
+  }
+  next();
+};
 
+module.exports = isAdmin;
--- src/middlewares/isTeacher.js
+++ src/middlewares/isTeacher.js
@@ -1,2 +1,9 @@
+const isTeacher = async (req, res, next) => {
+  const data = req.verified;
+  if (data?.role != "teacher") {
+    return res.status(400).json({ message: "You are not teacher" });
+  }
+  next();
+};
 
-  
+module.exports = isTeacher;
